b74368b3d2ff07eb1a26c0d8e524fde70b47bc85
unknown <0xnanoquetz9l@gmail.com>
2014-04-17 13:44:17 -0700

    updated .c file

--- k&r/hello_world.c
+++ k&r/hello_world.c
@@ -1,10 +1,12 @@
-/* tells the compiler to include information about the standard input/output library */
+//tells the compiler to include information about the standard input/output library
 #include <stdio.h>
 
-/* c programs begin executing at the beginning of
-main. every program must have a main somewhere. */
-main() 
+// c programs begin executing at the beginning of
+// main. every program must have a main somewhere.
+// defined a function called main that received no argument values.
+main()
 
-{
-	printf("hello, world!\n");
-}
+{                                // statements of main are enclosed in braces
+	printf("hello, world!\n");   // main calls library function printf
+                                 // to print this sequence of characters
+}                                // \n represents the newline character
