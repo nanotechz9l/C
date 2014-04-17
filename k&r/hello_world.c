961cc65cf04a1990f9df7612b46d6b43f4fa87a2
unknown <0xnanoquetz9l@gmail.com>
2014-04-17 13:19:07 -0700

    adding k&r folder

---
+++ k&r/hello_world.c
@@ -0,0 +1,10 @@
+/* tells the compiler to include information about the standard input/output library */
+#include <stdio.h>
+
+/* c programs begin executing at the beginning of
+main. every program must have a main somewhere. */
+main() 
+
+{
+	printf("hello, world!\n");
+}
